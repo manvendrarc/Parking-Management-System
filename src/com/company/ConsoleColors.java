fa194f5e19813e6d305d4ed84727474497e0f88d
Manvendra Singh Chauhan <78345394+manvendrarc@users.noreply.github.com>
2023-12-15 00:18:52 +0530

    Completed

---
+++ src/com/company/ConsoleColors.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ConsoleColors {
+    // Reset
+    public static final String RESET = "\033[0m"; // Text Reset
+
+    // Regular Colors
+    public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+    // High Intensity
+    public static final String WHITE_BRIGHT = "\033[0;97m"; // WHITE
+
+    // Bold High Intensity
+
+    public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+
+    // High Intensity backgrounds
+    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m"; // WHITE
+}
